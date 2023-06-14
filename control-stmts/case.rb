f51244b9bae672fbdf69667a476c01292690a79b
Jayson Mulwa <jayson.mulwa@gmail.com>
2023-06-14 21:18:37 +0300

    Update case.rb

--- control-stmts/case.rb
+++ control-stmts/case.rb
@@ -11,3 +11,26 @@ def gender(isMale)
 end
 
 puts gender(3)
+
+
+def day(character)
+    case character 
+    when "M"
+        return "Monday"
+    when "T"
+        return "Tuesday"
+    when "W"
+        return "Wednesday"
+    when "H"
+        return "Thursday"
+    when "F"
+        return "Friday"
+    when "S"
+        return "Saturday"
+    when "U"
+        return "Sunday"
+    else
+        return "Invalid abbreviation"
+    end
+end
+puts day()
